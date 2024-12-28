c8d079a0f911358e08155aa06d08f035c4b3cb1f
StatisticShock <cleyton.o.legal+github@gmail.com>
2024-12-28 02:20:54 -0300

    test

--- script.js
+++ script.js
@@ -211,6 +211,7 @@ async function addImages () {
             data.forEach(row => {   //Splits each line by ';' characters
                 splitData.push([row.split(/;/)]);
             });
+            console.log(splitData);
             return splitData
         } catch (error) {
             console.error(error.message)
