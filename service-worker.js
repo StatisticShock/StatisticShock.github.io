027b1f0aa4c67f2eca84e6529e7dc9046957159b
StatisticShock <cleyton.o.legal+github@gmail.com>
2025-10-26 13:31:08 -0300

    add cache to webp images

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,23 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('my-cache').then((cache) => {
-      return cache.addAll(['/', '/index.html', '/style.css', '/src/script.js']);
-    })
-  );
+self.addEventListener('install', (ev) => {
+	ev.waitUntil(
+		caches.open('index-cache').then((cache) => {
+			return cache.addAll(['/', '/index.html', '/style.css', '/src/script.js', '/util/functions.js']);
+		})
+	);
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (ev) => {
+	const req = ev.request;
+
+	if (req.url.endsWith('.webp')) {
+		ev.respondWith(
+			fetch(req).then(res => {
+				const clone = res.clone();
+				caches.open(index_cache).then(c => c.put(req, clone));
+				return res;
+			}).catch(() => caches.match(req))
+		);
+	} else {
+		ev.respondWith(caches.match(req).then(cached => cached || fetch(req)));
+	}
 });
