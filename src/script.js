8fc7e5dfe968454df3f7fd65bec7d241a7e47767
StatisticShock <cleyton.o.legal@gmail.com>
2025-12-07 09:49:31 -0300

    Fix double popup handling

--- src/script.js
+++ src/script.js
@@ -427,65 +427,6 @@ var UserInterface = /** @class */ (function () {
         keywordsWikipedia.value = '';
     };
     ;
-    UserInterface.showPopUps = function () {
-        var _this = this;
-        var popUpShortcuts = [];
-        document.querySelectorAll('form.pop-up').forEach(function (form) {
-            if (form.classList.length < 2)
-                return;
-            var otherClass = Array.from(form.classList).filter(function (className) { return className !== 'pop-up'; })[0];
-            var openBttn = Array.from(document.querySelectorAll(".".concat(otherClass))).filter(function (element) { return element.classList.contains('pop-up-open'); })[0];
-            popUpShortcuts.push({ button: openBttn, popUpContainer: form });
-        });
-        popUpShortcuts.forEach(function (object) {
-            var popUpClass = document.querySelectorAll('.pop-up');
-            var floatingLabelElement = object.popUpContainer.querySelectorAll('.floating-label');
-            object.button.onclick = function () {
-                var display = object.popUpContainer.style.display;
-                if ((display == '') || (display == 'none')) {
-                    object.popUpContainer.style.display = 'block';
-                }
-                else if (!(object.popUpContainer.classList.contains('create-shortcut') && object.button.classList.contains('create-shortcut') && !(object.button.id.replace('-button', '-item') === object.popUpContainer.getAttribute('x')))) {
-                    object.popUpContainer.style.display = 'none'; //Makes the popUp disappear
-                }
-                ;
-                popUpClass.forEach(function (element) {
-                    if (element != object.popUpContainer) {
-                        element.style.display = 'none';
-                    }
-                    ;
-                });
-                setTimeout(function () {
-                    floatingLabelElement.forEach(function (label) {
-                        var parent = label.parentElement;
-                        var siblings = Array.from(parent.children);
-                        var input = siblings[siblings.indexOf(label) - 1];
-                        var rect = object.popUpContainer.getBoundingClientRect();
-                        var inputRect = input.getBoundingClientRect();
-                        var left = inputRect.left - rect.left;
-                        label.style.left = '5px';
-                        input.placeholder ? input.placeholder = input.placeholder : input.placeholder = ' ';
-                    });
-                }, 10);
-            };
-            object.popUpContainer.addEventListener('keydown', function (e) {
-                if (e.key === 'Enter') {
-                    e.preventDefault();
-                    var okButton = object.popUpContainer.querySelector('.ok-button');
-                    okButton.click();
-                }
-            });
-        });
-        var buttons = document.querySelectorAll('.close-button');
-        buttons.forEach(function (button) {
-            var parent = button.parentElement.parentElement;
-            button.onclick = function () {
-                parent.style.display = 'none';
-                _this.setPopUpDefaultValues();
-            };
-        });
-    };
-    ;
     return UserInterface;
 }());
 ;
@@ -973,7 +914,6 @@ function onLoadFunctions(ev) {
                     UserInterface.dragPopUps();
                     UserInterface.setPopUpDefaultValues();
                     UserInterface.resetPopUpsOnOpen();
-                    UserInterface.showPopUps();
                     UserInterface.resizeHeader();
                     UserInterface.collapseHeader();
                     return [4 /*yield*/, CustomFunctions.sleep(300)];
--- src/script.ts
+++ src/script.ts
@@ -364,75 +364,6 @@ class UserInterface {
 
 		keywordsWikipedia.value = '';
 	};
-
-	static showPopUps(): void { //NO NEED OF RESPONSIVENESS
-		type popUpInterface = { button: HTMLAnchorElement | HTMLButtonElement, popUpContainer: HTMLFormElement }
-		const popUpShortcuts: Array<popUpInterface> = [];
-
-		(document.querySelectorAll('form.pop-up') as NodeListOf<HTMLFormElement>).forEach((form) => {
-			if (form.classList.length < 2) return;
-
-			const otherClass: string = Array.from(form.classList).filter((className) => className !== 'pop-up')[0];
-
-			const openBttn = Array.from(document.querySelectorAll(`.${otherClass}`)).filter((element) => element.classList.contains('pop-up-open'))[0] as HTMLAnchorElement | HTMLButtonElement;
-
-			popUpShortcuts.push({ button: openBttn, popUpContainer: form });
-		});
-
-		popUpShortcuts.forEach((object) => {
-			let popUpClass = document.querySelectorAll('.pop-up') as NodeListOf<HTMLElement>;
-			let floatingLabelElement = object.popUpContainer.querySelectorAll('.floating-label') as NodeListOf<HTMLElement>;
-
-			object.button.onclick = () => {
-				let display: string = object.popUpContainer.style.display;
-
-				if ((display == '') || (display == 'none')) {
-					object.popUpContainer.style.display = 'block';
-				} else if (!(object.popUpContainer.classList.contains('create-shortcut') && object.button.classList.contains('create-shortcut') && !(object.button.id.replace('-button', '-item') === object.popUpContainer.getAttribute('x')))) {
-					object.popUpContainer.style.display = 'none';    //Makes the popUp disappear
-				};
-
-				popUpClass.forEach((element) => {   // Makes every other popUp disappear once the shorcut button is clicked
-					if (element != object.popUpContainer) {
-						element.style.display = 'none'
-					};
-				});
-
-				setTimeout(() => {
-					floatingLabelElement.forEach((label) => {
-						const parent = label.parentElement as HTMLElement;
-						const siblings = Array.from(parent.children) as Array<HTMLElement>;
-						const input = siblings[siblings.indexOf(label) - 1] as HTMLInputElement;
-						const rect = object.popUpContainer.getBoundingClientRect();
-						const inputRect = input.getBoundingClientRect();
-
-						const left = inputRect.left - rect.left;
-
-						label.style.left = '5px';
-
-						input.placeholder ? input.placeholder = input.placeholder : input.placeholder = ' ';
-					});
-				}, 10);
-			}
-
-			object.popUpContainer.addEventListener('keydown', function (e) {
-				if (e.key === 'Enter') {
-					e.preventDefault();
-					let okButton = object.popUpContainer.querySelector('.ok-button') as HTMLButtonElement;
-					okButton.click();
-				}
-			});
-		});
-
-		let buttons = document.querySelectorAll('.close-button') as NodeListOf<HTMLButtonElement>;
-		buttons.forEach((button) => {
-			let parent = button.parentElement!.parentElement as HTMLElement;
-			button.onclick = () => {
-				parent.style.display = 'none';
-				this.setPopUpDefaultValues();
-			};
-		})
-	};
 };
 
 class ExternalSearch {
@@ -850,7 +781,6 @@ window.addEventListener('load', onLoadFunctions, true); async function onLoadFun
 	UserInterface.dragPopUps();
 	UserInterface.setPopUpDefaultValues();
 	UserInterface.resetPopUpsOnOpen();
-	UserInterface.showPopUps();
 	UserInterface.resizeHeader();
 	UserInterface.collapseHeader();
 
